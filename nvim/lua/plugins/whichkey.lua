d9a79900a2577c90f639c435d61770a9f07cf7b8
faber <fabrice.charbonneau@hotmail.com>
2025-05-24 17:56:47 -0400

    switched to kitty, generative ai plugin working

---
+++ nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,37 @@
+return {
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		config = function() -- This is the function that runs, AFTER loading
+			local wk = require("which-key")
+
+			-- Document existing key chains
+			wk.add({
+
+				{ "<leader>N", group = "toggle line [N]umbers" },
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>c_", hidden = true },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>d_", hidden = true },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>g_", hidden = true },
+				{ "<leader>gc", group = "git [C]ommit or [C]heckout" },
+				{ "<leader>gc_", hidden = true },
+				{ "<leader>h", group = "Git [H]unk" },
+				{ "<leader>h_", hidden = true },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>r_", hidden = true },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>s_", hidden = true },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>t_", hidden = true },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>w_", hidden = true },
+			})
+			-- visual mode
+			wk.add({
+				{ "<leader>h", group = "Git [H]unk", mode = "v" },
+			})
+		end,
+	},
+}
